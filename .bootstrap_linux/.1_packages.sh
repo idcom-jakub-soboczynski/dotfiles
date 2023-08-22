7dcacc5b017f848cdb43d9ac513bc80b3ab22075
Jakub Soboczyński <jakub.soboczynski@idcom.pl>
2023-08-22 11:58:37 +0200

    2023-08-22 10:17:48: update dotfiles

---
+++ .bootstrap_linux/.1_packages.sh
@@ -0,0 +1,531 @@
+#!/usr/bin/env bash
+
+# File based on mathiasbynens and kentcdodds .macos files!
+# mathiasbynens — https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# kentcdodds — https://github.com/kentcdodds/dotfiles/blob/master/.macos
+
+# Run without downloading:
+# curl -s https://raw.githubusercontent.com/idcom-jakub-soboczynski/dotfiles/master/.linux_packages.sh | bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.linux_packages` has finished
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+###############################################################################
+# Tezivosky's Customizations                                                  #
+###############################################################################
+
+echo -e "\n\n"
+echo "##################################################"
+echo -e "\nHello $(whoami)! Let's set up your packages! 🔥\n"
+echo "##################################################"
+
+echo -e "\n"
+echo "System update..."
+echo "------------------------------------------------"
+
+sudo apt update -yqq && sudo apt upgrade -yqq
+echo "System updated 🔥"
+
+echo -e "\n"
+echo "Installing curl..."
+echo "------------------------------------------------"
+
+if ! command -v curl &>/dev/null; then
+  sudo apt install curl -yqq
+  echo "curl - installed 🔥"
+else
+  echo "curl - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing wget..."
+echo "------------------------------------------------"
+
+if ! command -v wget &>/dev/null; then
+  sudo apt install wget -yqq
+  echo "wget - installed 🔥"
+else
+  echo "wget - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing git..."
+echo "------------------------------------------------"
+
+if ! command -v git &>/dev/null; then
+  sudo apt install git -yqq
+  curl -s -L https://raw.githubusercontent.com/kamranahmedse/git-standup/master/installer.sh | sudo sh
+  echo "git - installed 🔥"
+else
+  echo "git - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing tree..."
+echo "------------------------------------------------"
+
+if ! command -v tree &>/dev/null; then
+  sudo apt install tree -yqq
+  echo "tree - installed 🔥"
+else
+  echo "tree - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing fdfind..."
+echo "------------------------------------------------"
+
+if ! command -v fdfind &>/dev/null; then
+  sudo apt install fdfind -yqq
+  ln -s $(which fdfind) ~/.local/bin/fd
+  echo "fdfind - installed 🔥"
+else
+  echo "fdfind - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing fzf..."
+echo "------------------------------------------------"
+
+if ! command -v fzf &>/dev/null; then
+  sudo apt install fzf -yqq
+  echo "fzf - installed 🔥"
+else
+  echo "fzf - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing snapd..."
+echo "------------------------------------------------"
+
+if ! command -v snap &>/dev/null; then
+  sudo mv /etc/apt/preferences.d/nosnap.pref ~/nosnap.backup
+  sudo apt update -yqq
+  sudo apt install snapd -yqq
+  echo "snapd - installed 🔥"
+else
+  echo "snapd - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing flatpak..."
+echo "------------------------------------------------"
+
+if ! command -v flatpak &>/dev/null; then
+  sudo apt install flatpak -yqq
+  echo "flatpak - installed 🔥"
+else
+  echo "flatpak - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing dropbox..."
+echo "------------------------------------------------"
+
+if ! command -v dropbox &>/dev/null; then
+  sudo apt install dropbox -yqq
+  echo "dropbox - installed 🔥"
+else
+  echo "dropbox - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing keepassxc..."
+echo "------------------------------------------------"
+
+if ! command -v keepassxc &>/dev/null; then
+  sudo apt-get install keepassxc -yqq
+  echo "keepassxc - installed 🔥"
+else
+  echo "keepassxc - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing slack..."
+echo "------------------------------------------------"
+
+if ! command -v slack &>/dev/null; then
+  sudo snap install slack
+  echo "slack - installed 🔥"
+else
+  echo "slack - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing kazam..."
+echo "------------------------------------------------"
+
+if ! command -v kazam &>/dev/null; then
+  sudo snap install kazam
+  echo "kazam - installed 🔥"
+else
+  echo "kazam - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing llama..."
+echo "------------------------------------------------"
+
+if ! command -v llama &>/dev/null; then
+  sudo snap install llama
+  echo "llama - installed 🔥"
+else
+  echo "llama - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing espanso..."
+echo "------------------------------------------------"
+
+if ! command -v espanso &>/dev/null; then
+  sudo snap install espanso --classic --channel=latest/edge
+  echo "espanso - installed 🔥"
+else
+  echo "espanso - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing pip3..."
+echo "------------------------------------------------"
+
+if ! command -v pip3 &>/dev/null; then
+  sudo apt-get install python3-pip -yqq
+  echo "pip3 - installed 🔥"
+else
+  echo "pip3 - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing google-chrome-stable..."
+echo "------------------------------------------------"
+
+if ! command -v google-chrome-stable &>/dev/null; then
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
+  sudo apt update -yqq
+  sudo apt install google-chrome-stable -yqq
+  echo "google-chrome-stable - installed 🔥"
+else
+  echo "google-chrome-stable - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing pip3 packets..."
+echo "------------------------------------------------"
+
+if command -v pip3 &>/dev/null; then
+  pip3 install wrapt-timeout-decorator -q
+  pip3 install case-converter -q
+  pip3 install Faker -q
+  pip3 install units -q
+  pip3 install pydbus -q
+  echo "Pip3 packets - installed 🔥"
+else
+  echo "Pip3 packets - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing gimp..."
+echo "------------------------------------------------"
+
+if ! command -v gimp &>/dev/null; then
+  sudo apt install gimp -yqq
+  echo "gimp - installed 🔥"
+else
+  echo "gimp - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing htop..."
+echo "------------------------------------------------"
+
+if ! command -v htop &>/dev/null; then
+  sudo apt install htop -yqq
+  echo "htop - installed 🔥"
+else
+  echo "htop - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing inkscape..."
+echo "------------------------------------------------"
+
+if ! command -v inkscape &>/dev/null; then
+  sudo snap install inkscape
+  echo "inkscape - installed 🔥"
+else
+  echo "inkscape - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing emote..."
+echo "------------------------------------------------"
+
+if ! command -v emote &>/dev/null; then
+  sudo snap install emote
+  echo "emote - installed 🔥"
+else
+  echo "emote - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing insomnia..."
+echo "------------------------------------------------"
+
+if ! command -v insomnia &>/dev/null; then
+  sudo snap install insomnia
+  echo "insomnia - installed 🔥"
+else
+  echo "insomnia - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing midnight commander..."
+echo "------------------------------------------------"
+
+if ! command -v mc &>/dev/null; then
+  sudo apt install mc -yqq
+  echo "midnight commander - installed 🔥"
+else
+  echo "midnight commander - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing ulauncher..."
+echo "------------------------------------------------"
+
+if ! command -v ulauncher &>/dev/null; then
+  sudo add-apt-repository ppa:agornostal/ulauncher
+  sudo apt update -yqq
+  sudo apt install ulauncher -yqq
+  echo "ulauncher - installed 🔥"
+else
+  echo "ulauncher - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing solaar..."
+echo "------------------------------------------------"
+
+if ! command -v solaar &>/dev/null; then
+  sudo add-apt-repository ppa:daniel.pavel/solaar
+  sudo apt-get update -yqq
+  sudo apt-get install solaar -yqq
+  echo "solaar - installed 🔥"
+else
+  echo "solaar - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing vim..."
+echo "------------------------------------------------"
+
+if ! command -v vim &>/dev/null; then
+  sudo apt install vim -yqq
+  echo "vim - installed 🔥"
+else
+  echo "vim - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing visual studio code..."
+echo "------------------------------------------------"
+
+if ! command -v code &>/dev/null; then
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+  sudo apt-get install apt-transport-https -yqq
+  sudo apt-get update -yqq
+  sudo apt-get install code -yqq
+  echo "visual studio code - installed 🔥"
+else
+  echo "visual studio code - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing xclip..."
+echo "------------------------------------------------"
+
+if ! command -v xclip &>/dev/null; then
+  sudo apt-get install xclip -yqq
+  echo "xclip - installed 🔥"
+else
+  echo "xclip - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing node..."
+echo "------------------------------------------------"
+
+if ! command -v node &>/dev/null; then
+  sudo apt update -yqq
+  sudo apt install software-properties-common apt-transport-https ca-certificates gnupg2 curl build-essential -yqq
+  curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+  sudo apt install nodejs -yqq
+  echo "node - installed 🔥"
+else
+  echo "node - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing npn check updates..."
+echo "------------------------------------------------"
+
+if ! command -v ncu &>/dev/null; then
+  sudo npm install --global npm-check-updates
+  echo "npn check updates - installed 🔥"
+else
+  echo "npn check updates - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing prettier..."
+echo "------------------------------------------------"
+
+if ! command -v prettier &>/dev/null; then
+  sudo npm install --global prettier
+  echo "prettier - installed 🔥"
+else
+  echo "prettier - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing whois..."
+echo "------------------------------------------------"
+
+if ! command -v whois &>/dev/null; then
+  sudo apt-get install whois -yqq
+  echo "whois - installed 🔥"
+else
+  echo "whois - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing jq..."
+echo "------------------------------------------------"
+
+if ! command -v jq &>/dev/null; then
+  sudo apt install jq -yqq
+  echo "jq - installed 🔥"
+else
+  echo "jq - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing batcat..."
+echo "------------------------------------------------"
+
+if ! command -v batcat &>/dev/null; then
+  sudo apt-get install bat -yqq
+  echo "batcat - installed 🔥"
+else
+  echo "batcat - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing gh..."
+echo "------------------------------------------------"
+
+if ! command -v gh &>/dev/null; then
+  type -p curl >/dev/null || (sudo apt update && sudo apt install curl -yqq)
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+    sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+    sudo apt update -yqq &&
+    sudo apt install gh -yqq
+  echo "gh - installed 🔥"
+else
+  echo "gh - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing docker..."
+echo "------------------------------------------------"
+
+if ! command -v docker &>/dev/null; then
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+  sudo apt update -yqq
+  sudo apt install docker-ce docker-ce-cli containerd.io -yqq
+  sudo curl -sL "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo usermod -aG docker $USER
+  echo "docker - installed 🔥"
+else
+  echo "docker - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing zsh..."
+echo "------------------------------------------------"
+
+if ! command -v zsh &>/dev/null; then
+  sudo apt install zsh -yqq
+  chsh -s $(which zsh)
+
+  git clone -q https://github.com/zsh-users/zsh-autosuggestions.git $ZSH/plugins/zsh-autosuggestions
+  git clone -q https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
+  echo "zsh - installed 🔥"
+else
+  echo "zsh - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing oh-my-zsh..."
+echo "------------------------------------------------"
+
+OMZ_DIR=~/.oh-my-zsh
+if ! [ -d "$OMZ_DIR" ] &>/dev/null; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "oh-my-zsh - installed 🔥"
+else
+  echo "oh-my-zsh - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing fc-list..."
+echo "------------------------------------------------"
+
+if ! command -v fc-list &>/dev/null; then
+  sudo apt-get install fontconfig -yqq
+  echo "fc-list - installed 🔥"
+else
+  echo "fc-list - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing fonts powerline..."
+echo "------------------------------------------------"
+
+if ! fc-list | grep -i powerline &>/dev/null; then
+  sudo apt-get install fonts-powerline -yqq
+  echo "fonts powerline - installed 🔥"
+else
+  echo "fonts powerline - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Installing custom fonts..."
+echo "------------------------------------------------"
+
+mkdir -p ~/.local/share/fonts
+customFonts=(Meslo Inconsolata JetBrainsMono FiraCode FiraMono Noto Overpass RobotoMono SpaceMono UbuntuMono)
+for i in "${customFonts[@]}"; do
+  if ! ls ~/.local/share/fonts | grep -i "$i" &>/dev/null; then
+    wget -qv https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"$i".zip -O ~/.local/share/fonts/"$i".zip
+    unzip -oq ~/.local/share/fonts/"$i".zip -d ~/.local/share/fonts
+    rm -rf ~/.local/share/fonts/*Windows*
+    rm -rf ~/.local/share/fonts/"$i".zip
+    echo "$i - installed 🔥"
+  else
+    echo "$i - already exists! 👌"
+  fi
+done
---
+++ .bootstrap_linux/.2_idcom.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash
+
+# File based on mathiasbynens and kentcdodds .macos files!
+# mathiasbynens — https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+# kentcdodds — https://github.com/kentcdodds/dotfiles/blob/master/.macos
+
+# Run without downloading:
+# curl -s https://raw.githubusercontent.com/idcom-jakub-soboczynski/dotfiles/master/.linux_idcom.sh | bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.linux_idcom` has finished
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+echo -e "\n"
+echo "Logging to Docker Hub..."
+echo "------------------------------------------------"
+
+if ! docker info | grep -i "username" &>/dev/null; then
+  read -p "DockerHub Login: " DOCKER_HUB_LOGIN
+  read -p "DockerHub Password: " DOCKER_HUB_PASSWORD
+  docker login -u "$DOCKER_HUB_LOGIN" -p "$DOCKER_HUB_PASSWORD"
+  echo "Docker Hub - logged in 🔥"
+else
+  echo "Docker Hub - already logged in! 👌"
+fi
+
+echo -e "\n"
+echo "Generating SSH Keys..."
+echo "------------------------------------------------"
+
+SSH_IDCOM_FILE=~/.ssh/id_idcom_rsa
+if ! [ -f "$SSH_IDCOM_FILE" ] &>/dev/null; then
+  ssh-keygen -t ed25519 -b 4096 -C "jakub.soboczynski@idcom.pl" -f "$SSH_IDCOM_FILE"
+  eval "$(ssh-agent -s)"
+  ssh-add "$SSH_IDCOM_FILE"
+  echo "SSH Key - generated 🔥"
+else
+  echo "SSH Key - already exists! 👌"
+fi
+
+echo -e "\n"
+echo "Loggin in Github CLI..."
+echo "------------------------------------------------"
+
+if ! gh auth status | grep -i "Logged in to github.com" &>/dev/null; then
+  gh auth login
+  echo "Github CLI - logged in 🔥"
+else
+  echo "Github CLI - already logged in! 👌"
+fi
+
+echo -e "\n"
+echo "Install Dotfiles..."
+echo "------------------------------------------------"
+
+if ! [ -d "$HOME/.dotfiles" ] &>/dev/null; then
+  git clone -q --bare git@github.com:idcom-jakub-soboczynski/dotfiles.git $HOME/.dotfiles
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout -f
+  /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME pull -f
+  source ~/.zshrc
+  echo "Dotfiles - installed 🔥"
+else
+  echo "Dotfiles - already exists! 👌"
+fi
+
+if ! command -v idcom &>/dev/null; then
+  mkdir -p ~/IDcom/DEVELOP
+  git clone git@github.com:IDcomGroup/pl.idcom.develop.config.git ~/IDcom/DEVELOP
+  chmod +x ~/IDcom/DEVELOP/pl.idcom.develop.config/bin/idcom
+  echo "IDcom - installed 🔥"
+else
+  echo "IDcom - already exists! 👌"
+fi
+
+if command -v idcom &>/dev/null; then
+  if idcom git ls | grep -i "Nie Sklonowany" &>/dev/null; then
+    idcom git clone
+  fi
+
+  idcom db init
+
+  if ! docker ps | grep -i "idcom" &>/dev/null; then
+    idcom docker up
+  fi
+  echo "IDcom repositories - installed 🔥"
+else
+  echo "IDcom repositories - already exists! 👌"
+fi
