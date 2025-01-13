8de19ba3dde2caee330b0f942afa12cea9afcbf6
Jakub Soboczyński <jakub.soboczynski@idcom.pl>
2025-01-13 09:34:17 +0100

    2025-01-13 08:41:18: update dotfiles

--- .scripts/arrange_windows.sh
+++ .scripts/arrange_windows.sh
@@ -17,26 +17,36 @@ move_and_resize_window() {
   fi
 }
 
-# Desktop Left
-## Google Chrome
+# Workspace 1
+## Desktop Left
+### Google Chrome
 move_and_resize_window "google-chrome.Google-chrome" 0 "0,0,0,1920,1280"
-## Brave Browser
+### Brave Browser
 move_and_resize_window "brave-browser.Brave-browser" 0 "0,0,0,1920,1280"
 
-## Webstorm
+## Desktop Right
+### Webstorm
 move_and_resize_window "jetbrains-webstorm.jetbrains-webstorm" 0 "0,1920,0,1920,1280"
-## Visual Studio Code
+### Visual Studio Code
 move_and_resize_window "code.Code" 0 0,1920,0,1920,1280
+### Cursor
+move_and_resize_window "cursor.Cursor" 0 0,1920,0,1920,1280
+### Zed
+move_and_resize_window "dev.zed.Zed.dev.zed.Zed" 0 0,1920,0,1920,1280
 
-# Desktop Right
-## Slack
+# Workspace 2
+## Desktop Left
+### Slack
 move_and_resize_window "slack.Slack" 1 "0,0,0,1920,1280"
-## Mozilla Thunderbird
-move_and_resize_window "Mail.thunderbird" 1 "0,0,0,1920,1280"
 
-## Alacritty
+### Alacritty
 move_and_resize_window "Alacritty.Alacritty" 1 "0,1920,0,1920,1280"
-## Wezterm
+### Wezterm
 move_and_resize_window "org.wezfurlong.wezterm.org.wezfurlong.wezterm" 1 "0,1920,0,1920,1280"
-## Warp
+### Warp
 move_and_resize_window "dev.warp.Warp.dev.warp.Warp" 1 "0,1920,0,1920,1280"
+
+# Workspace 3
+## Desktop Left
+### Mozilla Thunderbird
+move_and_resize_window "Mail.thunderbird" 2 "0,0,0,1920,1280"
